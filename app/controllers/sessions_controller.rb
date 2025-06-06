e1dd1529eed1e5c123f0e2ff82b6793017a51fb8
Abhi Udayshankar <abhi.udayshankar@kellogg.northwestern.edu>
2025-06-06 00:08:35 +0000

    Set up login and signup paths

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,25 @@ class SessionsController < ApplicationController
   end
 
   def create
+   @user = User.find_by({ "email" => params["email"] })
+    if @user != nil
+      if BCrypt::Password.new(@user["password"]) == params["password"]
+        session["user_id"] = @user["id"]
+        flash["notice"] = "Hello."
+        redirect_to "/places"  # Redirect to places, not posts
+      else
+        flash["notice"] = "Invalid"
+        redirect_to "/login"
+      end
+    else
+      flash["notice"] = "Invalid"
+      redirect_to "/login"
+    end
   end
 
   def destroy
+    session["user_id"] = nil
+    redirect_to login_path
   end
 end
   
