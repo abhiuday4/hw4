ca7b85ab596e053486e1ea749c70f7136065c7eb
Abhi Udayshankar <abhi.udayshankar@kellogg.northwestern.edu>
2025-06-06 01:42:30 +0000

    Completed user stories (minus optional image), authentication, & authorization

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -9,8 +9,12 @@ class PlacesController < ApplicationController
   end
 
   def show
-    @place = Place.find_by({ "id" => params["id"] })
-    @entries = Entry.where({ "place_id" => @place["id"] })
+     @place = Place.find_by({ "id" => params["id"] })
+  if session["user_id"]
+    @entries = Entry.where({ "place_id" => @place["id"], "user_id" => session["user_id"] })
+  else
+    @entries = []
+  end
   end
 
   def new
