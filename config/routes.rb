db63534d65bae25412ee192eac676621a0986829
Abhi Udayshankar <abhi.udayshankar@kellogg.northwestern.edu>
2025-06-06 00:14:19 +0000

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,5 @@ Rails.application.routes.draw do
 
   get("/login", { :controller => "sessions", :action => "new" })
   post("/login", { :controller => "sessions", :action => "create" })
-  delete("/logout", { :controller => "sessions", :action => "destroy" })
+  get("/logout", {:controller => "sessions", :action => "destroy"})
 end
